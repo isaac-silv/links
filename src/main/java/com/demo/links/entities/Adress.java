8e496404e00f0b435af7c5474e58274c1dbbe93c
isaac-silv <isaacaquino.dv@gmail.com>
2023-05-18 16:52:53 -0300

    first commit

---
+++ src/main/java/com/demo/links/entities/Adress.java
@@ -0,0 +1,129 @@
+package com.demo.links.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_adress")
+public class Adress {
+    
+    private Long id;
+    private String street;
+    private String adressNumber;
+    private String city;
+    private String state;
+    private String coutry;
+    private String postalCode;
+
+    @OneToOne
+    @JoinColumn(name = "establishment_id")
+    private Establishment establishment;
+
+    public Adress() {
+    }
+
+    public Adress(Long id, String street, String adressNumber, String city, String state, String coutry,
+            String postalCode, Establishment establishment) {
+        this.id = id;
+        this.street = street;
+        this.adressNumber = adressNumber;
+        this.city = city;
+        this.state = state;
+        this.coutry = coutry;
+        this.postalCode = postalCode;
+        this.establishment = establishment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getAdressNumber() {
+        return adressNumber;
+    }
+
+    public void setAdressNumber(String adressNumber) {
+        this.adressNumber = adressNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCoutry() {
+        return coutry;
+    }
+
+    public void setCoutry(String coutry) {
+        this.coutry = coutry;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Establishment getEstablishment() {
+        return establishment;
+    }
+
+    public void setEstablishment(Establishment establishment) {
+        this.establishment = establishment;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Adress other = (Adress) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }   
+    
+
+}
---
+++ src/main/java/com/demo/links/entities/Establishment.java
@@ -0,0 +1,126 @@
+package com.demo.links.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_establishment")
+public class Establishment {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private String phone;
+    private String urlPhoto;
+    private String urlCover;
+
+    @OneToOne(mappedBy = "establishment_id")
+    private Adress adress;
+
+    public Establishment() {
+    }
+
+    public Establishment(Long id, String name, String description, String phone, String urlPhoto, String urlCover,
+            Adress adress) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.phone = phone;
+        this.urlPhoto = urlPhoto;
+        this.urlCover = urlCover;
+        this.adress = adress;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUrlPhoto() {
+        return urlPhoto;
+    }
+
+    public void setUrlPhoto(String urlPhoto) {
+        this.urlPhoto = urlPhoto;
+    }
+
+    public String getUrlCover() {
+        return urlCover;
+    }
+
+    public void setUrlCover(String urlCover) {
+        this.urlCover = urlCover;
+    }
+
+    public Adress getAdress() {
+        return adress;
+    }
+
+    public void setAdress(Adress adress) {
+        this.adress = adress;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Establishment other = (Establishment) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+    
+}
